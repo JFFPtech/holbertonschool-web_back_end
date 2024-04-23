0f993e7f34a058e780c0c670cab7647ffece3333
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-23 05:48:36 -0700

    test ran fine

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies a float by multiplier"""
+    def multiply(n: float) -> float:
+        """Return the product of a float and multiplier"""
+        return n * multiplier
+    return multiply
