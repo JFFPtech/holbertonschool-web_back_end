714f152f5b2d22ac22b197796109ef974b49fc2e
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-04-23 08:26:11 -0400

    work from yesterday

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Type-annotated function floor that takes a float n as argument and returns the floor of the float."""
+
+import math
+
+def floor(n: float):
+    """Return the floor of a float"""
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Type-annotated function to_str that takes a float n as argument and returns the string representation of the float."""
+def to_str(n: float):
+    """Return the string representation of a float"""
+    return str(n)
