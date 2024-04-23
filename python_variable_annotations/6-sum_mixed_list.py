32b2100fa29a53296dd743fd02e41d36d4f7cc40
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-23 05:36:12 -0700

    tests running fine

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Type-annotated function sum_mixed_list that takes a list mxd_lst of integers and floats and returns their sum as a float."""
+
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]):
+    """Return the sum of a list of integers and floats"""
+    return sum(mxd_lst)
