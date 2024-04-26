85fe9c0faa6fd879cd15427ac6182cb876f29754
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-26 16:49:49 -0700

    test, prints out floats, missing annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,11 +3,8 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
 
-
-async def async_generator() -> AsyncGenerator[float, None]:
-    """ Yields a random number between 0 and 10 every second for 10 seconds"""
+async def async_generator():
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
