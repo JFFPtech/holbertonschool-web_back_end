ab20485b3c6ec5e3db1f2a3912cdca5b4de9f518
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-26 17:17:22 -0700

    syntax clarification

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Measure runtime
-"""
+""" Measure runtime """
 
 from asyncio import gather
 from time import time
@@ -11,8 +9,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Measure runtime"""
-    start = time()
+    start_time = time()
     tasks = [async_comprehension() for _ in range(4)]
     await gather(*tasks)
-    end = time()
-    return end - start
+    end_time = time()
+    return end_time - start_time
