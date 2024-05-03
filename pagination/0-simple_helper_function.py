025eb8a9071e73269d10c802c5ab474dd2400704
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-03 09:57:44 -0700

    pycode fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,7 +6,7 @@ from typing import Tuple
 def index_range(page, page_size: int) -> Tuple[int, int]:
     """ Calculates the index range to start """
     start_index = (page - 1) * page_size
-    
-    end_index = page + page_size - 1
-    
+
+    end_index = start_index + page_size - 1
+
     return (start_index, end_index)
