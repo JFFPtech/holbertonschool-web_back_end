4711314a1aff1d67c8265d5fcac34d5fe032be56
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-03 09:52:40 -0700

    added data type to func, annotated tuple, imported tuple

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Pagination with Python """
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page, page_size: int) -> Tuple[int, int]:
     """ Calculates the index range to start """
     start_index = (page - 1) * page_size
     
