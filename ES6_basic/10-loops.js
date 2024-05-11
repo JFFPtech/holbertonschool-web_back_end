3290f4e84031d48b5c262e08060568402b1c90ce
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-05-10 22:13:52 -0400

    slint

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
-export default function appendToEAchArrayValue(array, appendString) {
-    const arr = [];
-    for (const value of array) {
-      arr.push(appendString + value);
-    }
-    return arr;
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+  for (const value of array) {
+    arr.push(appendString + value);
   }
+  return arr;
+}
