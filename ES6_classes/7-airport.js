767eea4dd7849241718cf30655b429ec34f4bc2f
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-05-10 23:36:55 -0400

    changes to 7, added export at end

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,11 +1,12 @@
-export default class Airport {
+class Airport {
     constructor(name, code) {
       this._name = name;
       this._code = code;
     }
   
-    toString() {
-      return `[object ${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+    get [Symbol.toStringTag]() {
+      return this._code;
     }
   }
   
+  export default Airport;
