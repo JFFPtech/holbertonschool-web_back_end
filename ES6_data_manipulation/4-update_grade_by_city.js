a0c97e15e42d7a72f08e8d1f21647152f1b184ec
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-21 09:41:02 -0700

    passed eslint on 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,14 @@
 function updateStudentGradeByCity(students, city, grade) {
-    return students.map((student) => {
-        const newGrade = grade.find((item) => item.studentId === student.id);
-        if (newGrade) {
-        return {
-            ...student,
-            grade: newGrade.grade,
-        };
-        }
-        return student;
-    });
+  return students.map((student) => {
+    const newGrade = grade.find((item) => item.studentId === student.id);
+    if (newGrade) {
+      return {
+        ...student,
+        grade: newGrade.grade,
+      };
+    }
+    return student;
+  });
 }
 
 export default updateStudentGradeByCity;
